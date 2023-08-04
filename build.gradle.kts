0f005524c5ab66bdee5bec8573c75bd913e64cc4
vkuzel <vkuzel@gmail.com>
2023-08-04 07:19:36 +0200

    Gradle configuration.

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+	java
+}
+
+group = "com.vkuzel"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+}
+
